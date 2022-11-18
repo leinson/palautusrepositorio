1a0f299f409b4bdd6d445399e5526bbe51dded9b
leinson <hannah.leinson@gmail.com>
2022-11-18 14:51:18 +0200

    nhl-pelaajatulosten muokkaamista ja refaktorointia, tehtävät 1-3

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
         self.name = name
-    
+        self.nationality=nationality
+        self.assists=assists
+        self.goals=goals
+        self.penalties=penalties
+        self.team=team
+        self.games=games
+
     def __str__(self):
-        return self.name
+        return f"{self.name:20} {self.team} {self.goals:5} + {self.assists:<5} = {self.goals+self.assists:<5}"
