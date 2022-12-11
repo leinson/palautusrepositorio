bb6299d179274d2c8cb8843edbac6302ed94243d
leinson <hannah.leinson@gmail.com>
2022-12-11 16:33:43 +0200

    tehtävät 2-3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,10 +1,10 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
 
 def main():
-    url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
-    reader = PlayerReader(url)
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
+    reader = PlayerReader(url) #Erottelee pelaajat player olioiksi, sijoittaa listaan & palauttaa listan.
     stats = Statistics(reader)
 
     matcher = And(
@@ -12,10 +12,48 @@ def main():
         HasAtLeast(5, "assists"),
         PlaysIn("PHI")
     )
-
     for player in stats.matches(matcher):
         print(player)
 
+    print("ALL")
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
 
+    print("NOT")
+    matcher = And(
+        Not(HasAtLeast(1, "goals")),
+        PlaysIn("NYR")
+        )
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("HAS FEWER THAN")
+    matcher = And(
+        HasFewerThan(1, "goals"), 
+        PlaysIn("NYR")
+        )
+    for player in stats.matches(matcher):
+        print(player)
+   
+    print("OR - HAS AT LEAST")
+    matcher = Or(
+        HasAtLeast(45, "goals"), 
+        HasAtLeast(70, "assists")
+        )
+    for player in stats.matches(matcher):
+        print(player)
+    
+    print("AND + OR")
+    matcher = And(
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("NYR"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+        )
+    )
+    for player in stats.matches(matcher):
+        print(player)
+        
 if __name__ == "__main__":
     main()
